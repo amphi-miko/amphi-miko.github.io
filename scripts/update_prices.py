fbe8940671c69c22895fba7ab32ff07ee47b76ec
miko <105166653+amphi-miko@users.noreply.github.com>
2026-02-08 17:36:50 -0500

    Update update_prices.py

--- scripts/update_prices.py
+++ scripts/update_prices.py
@@ -23,7 +23,7 @@ item_info = {
 }
 prices = {}
 
-for item in item_names_ids:
+for item in item_info:
     API_URL = f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={item_info[item]['id']}"  # <-- your real API URL here
     # Fetch data from API
     response = requests.get(API_URL, timeout=10)
