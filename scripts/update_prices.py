c4dfa7e5a88046aa03aa86a7318241ae5f3a6d47
miko <105166653+amphi-miko@users.noreply.github.com>
2026-02-08 16:34:44 -0500

    Update update_prices.py

--- scripts/update_prices.py
+++ scripts/update_prices.py
@@ -1,21 +1,27 @@
 import json
 import requests
+import time
 from pathlib import Path
 from datetime import datetime, timezone
 
-API_URL = "https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid=176482232"  # <-- your real API URL here
-# Fetch data from API
-response = requests.get(API_URL, timeout=10)
-response.raise_for_status()
-
-data = response.json()
-# Extract price (adjust this key if needed)
-price = int(data["highest_buy_order"])
-
-# Example prices (replace with API results)
-prices = {
-    "Santa Hat": price,
+item_names_ids = {
+    "Santa Hat":176482232
+    "Santa Jacket":176481264
+    "Santa Trousers":176482233
 }
+prices = {}
+
+for item in item_names_ids:
+    API_URL = f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={item_names_ids[item]}"  # <-- your real API URL here
+    # Fetch data from API
+    response = requests.get(API_URL, timeout=10)
+    response.raise_for_status()
+    data = response.json()
+    # Extract price (adjust this key if needed)
+    price = int(data["highest_buy_order"])
+    # Example prices (replace with API results)
+    prices[item] = price
+    time.sleep(1)
 
 data = {
     "updated": datetime.now(timezone.utc).isoformat(),
