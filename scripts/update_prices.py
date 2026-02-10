9719d3954cf1a4edd70f242a6f7fe436f06b84be
amphi-miko <bobilliambob@yahoo.ca>
2026-02-10 00:35:55 -0500

    Added icons, included all item info in json file

--- scripts/update_prices.py
+++ scripts/update_prices.py
@@ -4,31 +4,13 @@ import time
 from pathlib import Path
 from datetime import datetime, timezone
 
-item_info = {
-    "Face Tattoos":{"id":176478961,"qty":146},
-    "Neck Tattoos":{"id":176478409,"qty":136},
-    "Santa Hat":{"id":176482232,"qty":3235},
-    "Santa Jacket":{"id":176481264,"qty":2075},
-    "Santa Trousers":{"id":176482233,"qty":1968},
-    "Valentine Bear Head":{"id":176492719,"qty":2906},
-    "Valentines Bear Top":{"id":176492722,"qty":2276},
-    "Valentines Bear Trousers":{"id":176492721,"qty":2173},
-    "Easter Bonnet":{"id":176508769,"qty":3514},
-    "Easter Dungarees":{"id":176508770,"qty":2184},
-    "Easter Slippers":{"id":176508771,"qty":3195},
-    "Wizard Hat":{"id":176542129,"qty":7316},
-    "Wizard Beard":{"id":176542131,"qty":7384},
-    "Wizard Gown":{"id":176542130,"qty":6189},
-    "Wizard Trousers":{"id":176542132,"qty":6035}
-}
-
-with open("prices.json", "r", encoding="utf-8") as f:
+with open("item_info.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 current_time_str = datetime.now(timezone.utc).isoformat()
 
-for item in item_info:
-    API_URL = f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={item_info[item]['id']}"  # <-- your real API URL here
+for item in data:
+    API_URL = f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={data[item]['id']}"  # <-- your real API URL here
     # Fetch data from API
     response = requests.get(API_URL, timeout=10)
     response.raise_for_status()
@@ -36,16 +18,17 @@ for item in item_info:
     # Extract price (adjust this key if needed)
     bid = int(orderbook_data["highest_buy_order"])
     ask = int(orderbook_data["lowest_sell_order"])
-    mcap = item_info[item]["qty"]*bid
-    if item in data["items"]:
-        data["items"][item]["history"].append({"timestamp":current_time_str,"ask":ask,"bid":bid,"mcap":mcap})
-        if len(data["items"][item]["history"]) >= 13:
-            data["items"][item]["history"].pop(0)
+    listed = int(orderbook_data["sell_order_summary"].split('</span>')[0].split(">")[1])
+    mcap = data[item]["qty"]*bid
+    if "history" in data[item]:
+        data[item]["history"].append({"timestamp":current_time_str,"ask":ask,"bid":bid,"mcap":mcap,"qty":listed})
+        if len(data[item]["history"]) >= 13:
+            data[item]["history"].pop(0)
     else:
-        data["items"][item] = {"history":[{"timestamp":current_time_str,"ask":ask,"bid":bid,"mcap":mcap}]}
-    time.sleep(3)
+        data[item]["history"] = [{"timestamp":current_time_str,"ask":ask,"bid":bid,"mcap":mcap,"qty":listed}]
+    time.sleep(5)
 
-output_file = Path("prices.json")
+output_file = Path("item_info.json")
 output_file.write_text(json.dumps(data, indent=2))
 
-print("Updated prices.json")
+print("Updated item_info.json")
